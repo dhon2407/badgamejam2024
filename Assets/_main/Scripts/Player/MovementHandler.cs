342a108b45d59153cc0ce9e1241b4e11743d9393
Dan <jessiedaniel7@yahoo.com>
2024-09-04 08:46:36 +0900

    Simple movement

---
+++ Assets/_main/Scripts/Player/MovementHandler.cs
@@ -0,0 +1,30 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game.Main.Player
+{
+    [HideMonoScript]
+    public class MovementHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D rb;
+        
+        [SerializeField]
+        private float moveSpeed = 5f; 
+
+        public bool Enabled { get; set; }
+        
+        private Vector2 _movement;
+
+        private void Update()
+        {
+            if (!Enabled)
+                return;
+            
+            _movement.x = Input.GetAxisRaw("Horizontal");
+            _movement.y = Input.GetAxisRaw("Vertical");
+
+            rb.MovePosition(rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
---
+++ Assets/_main/Scripts/Player/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game.Main.Player
+{
+    [HideMonoScript]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private MovementHandler movement;
+
+        private void Start()
+        {
+            movement.Enabled = true;
+        }
+    }
+}
